8f55981dcf16f33f775e88b1b9ee3fc38c63af87
samon11 <mjsamon@icloud.com>
2018-09-26 20:09:23 -0600

    bug fix

--- art_model.py
+++ art_model.py
@@ -32,14 +32,7 @@ train_datagen = ImageDataGenerator(
         fill_mode='nearest')
 
 test_datagen = ImageDataGenerator(
-        rescale=1./255,
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest')
+        rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
         train_dir,
